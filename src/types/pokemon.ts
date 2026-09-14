6f79a4e3023bbb171824ec80c17763e79a6393dc
Alejandro Marin Morales <trabajosadso@gmail.com>
2026-09-14 12:12:12 -0500

    Creacion de la carptea types y archivo pokemon.ts con su codigo

---
+++ src/types/pokemon.ts
@@ -0,0 +1,33 @@
+export interface PokemonListItem {
+  name: string;
+  url: string;
+}
+
+export interface PokemonListResponse {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: PokemonListItem[];
+}
+
+export interface PokemonType {
+  slot: number;
+  type: { name: string; url: string };
+}
+
+export interface PokemonStat {
+  base_stat: number;
+  stat: { name: string };
+}
+
+export interface PokemonDetail {
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  sprites: {
+    front_default: string | null;
+  };
+  types: PokemonType[];
+  stats: PokemonStat[];
+}
