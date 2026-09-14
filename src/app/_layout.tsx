798ccd5b5672c75f6bd01f32279245abc691d5ab
Alejandro Marin Morales <trabajosadso@gmail.com>
2026-09-14 12:02:01 -0500

    Configuración de navegación Stack y rutas en Expo Router

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
-}
+const RootLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Pokedex Lite" }} />
+      <Stack.Screen name="pokemon/[name]" options={{ title: "Detalle" }} />
+    </Stack>
+  );
+};
+
+export default RootLayout;
